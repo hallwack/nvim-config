f82bd109510abd9b1b1d1a95729289e767706b63
Raihan Adam <hallwack.id@gmail.com>
2023-08-14 10:34:43 +0700

    fix: split completion and lsp-zero in different file

--- lua/hallwack/plugins/lsp.lua
+++ lua/hallwack/plugins/lsp.lua
@@ -1,40 +1,21 @@
 return {
-	"VonHeikemen/lsp-zero.nvim",
-	branch = "v2.x",
+	"neovim/nvim-lspconfig",
+	cmd = 'LspInfo',
+	event = { 'BufReadPre', 'BufNewFile' },
 	dependencies = {
-		"neovim/nvim-lspconfig",
-		{
-			"williamboman/mason.nvim",
-			build = function()
-				pcall(vim.cmd, "MasonUpdate")
-			end,
-		},
+		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
 		"jose-elias-alvarez/null-ls.nvim",
-		"hrsh7th/nvim-cmp",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"saadparwaiz1/cmp_luasnip",
 		"onsails/lspkind-nvim",
 		{ "glepnir/lspsaga.nvim", event = "LspAttach" },
 		"jose-elias-alvarez/typescript.nvim",
 		"simrat39/rust-tools.nvim",
-		"L3MON4D3/LuaSnip",
-		"rafamadriz/friendly-snippets",
-		{
-			"dsznajder/vscode-es7-javascript-react-snippets",
-			build = "yarn install --frozen-lockfile && yarn compile",
-		},
 	},
 	config = function()
-		local ok_lspzero, lspzero = pcall(require, "lsp-zero")
+		local ok_lspzero, lsp = pcall(require, "lsp-zero")
 		local ok_lsp, lspconfig = pcall(require, "lspconfig")
 		local ok_mason, mason = pcall(require, "mason")
 		local ok_mason_lsp, masonlsp = pcall(require, "mason-lspconfig")
-		local ok_cmp, cmp = pcall(require, "cmp")
-		local ok_lspkind, lspkind = pcall(require, "lspkind")
-		local ok_luasnip, luasnip = pcall(require, "luasnip")
 		local ok_null_ls, null_ls = pcall(require, "null-ls")
 		local ok_lspsaga, lspsaga = pcall(require, "lspsaga")
 		local ok_ts, ts = pcall(require, "typescript")
@@ -46,9 +27,6 @@ return {
 					and ok_lsp
 					and ok_mason
 					and ok_mason_lsp
-					and ok_cmp
-					and ok_lspkind
-					and ok_luasnip
 					and ok_null_ls
 					and ok_lspsaga
 					and ok_ts
@@ -67,7 +45,6 @@ return {
 			ensure_installed = { "lua_ls", "tsserver", "tailwindcss", "prismals", "html", "cssls" },
 		})
 
-		local lsp = lspzero.preset({})
 		lspsaga.setup({
 			ui = {
 				theme = "round",
@@ -171,95 +148,18 @@ return {
 			sources = {
 				require("typescript.extensions.null-ls.code-actions"),
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettierd,
-				null_ls.builtins.formatting.eslint_d,
+				null_ls.builtins.formatting.prettierd.with({
+					condition = function(utils)
+						return utils.root_has_file({ ".prettierrc" })
+					end,
+				}),
+				null_ls.builtins.formatting.eslint_d.with({
+					prefer_local = "node_modules/.bin",
+					condition = function(utils)
+						return utils.root_has_file({ ".eslintrc.js" })
+					end,
+				}),
 				null_ls.builtins.formatting.blade_formatter,
-			},
-		})
-
-		require("luasnip/loaders/from_vscode").lazy_load()
-
-		local has_words_before = function()
-			unpack = unpack or table.unpack
-			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-		end
-
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(),
-				["<C-s>"] = cmp.mapping.select_prev_item(),
-				["<C-j>"] = cmp.mapping.select_next_item(),
-				["<C-d>"] = cmp.mapping.select_next_item(),
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping({
-					i = cmp.mapping.abort(),
-					c = cmp.mapping.close(),
-				}),
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expandable() then
-						luasnip.expand()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					elseif has_words_before() then
-						cmp.complete()
-					else
-						fallback()
-					end
-				end, {
-					"i",
-					"s",
-				}),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, {
-					"i",
-					"s",
-				}),
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-			}, {
-				{ name = "buffer" },
-				{ name = "path" },
-			}),
-			formatting = {
-				format = lspkind.cmp_format({
-					with_text = true,
-					maxwidth = 100,
-					-- menu = ({
-					--   buffer = "[Buffer]",
-					--   nvim_lsp = "[LSP]",
-					--   luasnip = "[LuaSnip]",
-					--   nvim_lua = "[Lua]",
-					--   latex_symbols = "[Latex]",
-					-- })
-				}),
-			},
-			confirm_opts = {
-				behavior = cmp.ConfirmBehavior.Replace,
-				select = false,
 			},
 		})
 	end,
