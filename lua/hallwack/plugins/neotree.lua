c753417eedc72b7569d0f78161ab97713c39d3d7
Raihan Adam <hallwack.id@gmail.com>
2023-08-14 10:35:44 +0700

    feat: add window-picker package (error cannot using window picker)

--- lua/hallwack/plugins/neotree.lua
+++ lua/hallwack/plugins/neotree.lua
@@ -4,6 +4,31 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
+    {
+      -- only needed if you want to use the commands with "_with_window_picker" suffix
+      's1n7ax/nvim-window-picker',
+      name = 'window-picker',
+      event = 'VeryLazy',
+      version = '2.*',
+      config = function()
+        require 'window-picker'.setup({
+          hint = 'floating-big-letter',
+          autoselect_one = true,
+          include_current = false,
+          filter_rules = {
+            -- filter using buffer options
+            bo = {
+              -- if the file type is one of following, the window will be ignored
+              filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+
+              -- if the buffer type is one of following, the window will be ignored
+              buftype = { 'terminal', "quickfix" },
+            },
+          },
+          other_win_hl_color = '#e35e4f',
+        })
+      end,
+    }
   },
   config = function()
     local ok_neotree, neotree = pcall(require, "neo-tree")
