90c026b9a0a0753ed3f6256aafc94847d200fa27
Raihan Adam <hallwack.id@gmail.com>
2023-08-30 17:29:20 +0700

    fix: change keymaps for nvimtree

--- lua/hallwack/keymaps.lua
+++ lua/hallwack/keymaps.lua
@@ -1,7 +1,7 @@
 local map = vim.keymap.set
 local default_opts = { noremap = true, silent = true }
 
-map("n", "<C-b>", ":Neotree toggle<CR>", default_opts)
+map("n", "<C-b>", ":NvimTreeToggle toggle<CR>", default_opts)
 map("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", default_opts)
 map("n", "<leader>gr", "<cmd>lua require('telescope.builtin').live_grep()<cr>", default_opts)
 map("n", "<leader>bf", "<cmd>lua require('telescope.builtin').buffers()<cr>", default_opts)
