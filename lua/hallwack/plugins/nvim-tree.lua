8a3d65a61a8073eeb8e1d09b439f3f1b705b3d5a
Raihan Adam <hallwack.id@gmail.com>
2023-08-30 17:29:10 +0700

    fix: change neo tree to nvim-tree

---
+++ lua/hallwack/plugins/nvim-tree.lua
@@ -0,0 +1,61 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+		cmd = { "NvimTreeToggle" },
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local nvimtree = require("nvim-tree")
+		local WIDTH_RATIO = 0.3
+		local HEIGHT_RATIO = 0.8
+
+		-- recommended settings from nvim-tree documentation
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+
+		-- change color for arrows in tree to light blue
+		vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
+
+		-- configure nvim-tree
+		nvimtree.setup({
+			view = {
+				float = {
+					enable = true,
+					open_win_config = function()
+						local screen_w = vim.opt.columns:get()
+						local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+						local window_w = screen_w * WIDTH_RATIO
+						local window_h = screen_h * HEIGHT_RATIO
+						local window_w_int = math.floor(window_w)
+						local window_h_int = math.floor(window_h)
+						local center_x = (screen_w - window_w) / 2
+						local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+						return {
+							border = "rounded",
+							relative = "editor",
+							row = center_y,
+							col = center_x,
+							width = window_w_int,
+							height = window_h_int,
+						}
+					end,
+				},
+			},
+			-- change folder arrow icons
+			renderer = {
+				icons = {
+					glyphs = {
+						folder = {
+							arrow_closed = "", -- arrow when folder is closed
+							arrow_open = "", -- arrow when folder is open
+						},
+					},
+				},
+			},
+			filters = {
+				custom = { ".DS_Store" },
+			},
+			git = {
+				ignore = false,
+			},
+		})
+	end,
+}
