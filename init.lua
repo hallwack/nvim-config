56c1ba2c24dcc474848990de0588c795b959e13d
Raihan Adam <hallwack.id@gmail.com>
2023-10-08 13:10:22 +0700

    fix: fixing import

--- init.lua
+++ init.lua
@@ -12,4 +12,4 @@ if not vim.loop.fs_stat(lazypath) then
 	})
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("hallwack.plugins")
+require("lazy").setup({ { import = "hallwack.plugins.lsp" }, { import = "hallwack.plugins" }, })
