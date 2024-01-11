3dbfe1315e4c8796e95abec72f874f8960d6c94f
Raihan Adam <hallwack.id@gmail.com>
2024-01-11 19:20:37 +0700

    feat: add harpoon

---
+++ lua/hallwack/plugins/harpoon.lua
@@ -0,0 +1,45 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { { "nvim-lua/plenary.nvim" } },
+	config = function()
+		local harpoon = require("harpoon")
+
+		harpoon:setup()
+
+		local conf = require("telescope.config").values
+		local function toggle_telescope(harpoon_files)
+			local file_paths = {}
+			for _, item in ipairs(harpoon_files.items) do
+				table.insert(file_paths, item.value)
+			end
+
+			require("telescope.pickers")
+				.new({}, {
+					prompt_title = "Harpoon",
+					finder = require("telescope.finders").new_table({
+						results = file_paths,
+					}),
+					previewer = conf.file_previewer({}),
+					sorter = conf.generic_sorter({}),
+				})
+				:find()
+		end
+
+		vim.keymap.set("n", "<leader>th", function()
+			toggle_telescope(harpoon:list())
+		end, { desc = "Open harpoon window" })
+		vim.keymap.set("n", "<leader>ha", function()
+			harpoon:list():append()
+		end, { desc = "Append harpoon list" })
+		vim.keymap.set("n", "<leader>hr", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+		vim.keymap.set("n", "<leader>zz", function()
+			harpoon:list():prev()
+		end)
+		vim.keymap.set("n", "<leader>cc", function()
+			harpoon:list():next()
+		end)
+	end,
+}
