e26013e5a9b72560f55481d3b0ce5c2a5334b075
Raihan Adam <hallwack.id@gmail.com>
2024-03-05 16:25:25 +0700

    feat: creating linting lua config

---
+++ lua/hallwack/plugins/lsp/linting.lua
@@ -0,0 +1,28 @@
+return {
+	"mfussenegger/nvim-lint",
+	event = {
+		"BufReadPre",
+		"BufNewFile",
+	},
+	config = function()
+		local lint = require("lint")
+
+		lint.linters_by_ft = {
+			javascript = { "quick-lint-js" },
+			typescript = { "quick-lint-js" },
+			javascriptreact = { "quick-lint-js" },
+			typescriptreact = { "quick-lint-js" },
+			svelte = { "eslint_d" },
+			python = { "pylint" },
+		}
+
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
+}
