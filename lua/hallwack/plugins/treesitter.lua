cfe7c39e6ef1bd0cf4789ad5fb6e1e10ae26646f
Raihan Adam <hallwack.id@gmail.com>
2024-03-05 16:36:21 +0700

    fix: fix formatting and add nvim treesitter context plugins

--- lua/hallwack/plugins/treesitter.lua
+++ lua/hallwack/plugins/treesitter.lua
@@ -1,41 +1,44 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+		{
+			"nvim-treesitter/nvim-treesitter-context",
+		},
+	},
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
 
-    treesitter.setup({
-      ensure_installed = {
-        "cpp",
-        "python",
-        "lua",
-        "java",
-        "javascript",
-        "typescript",
-        "html",
-        "css",
-        "php",
-        "tsx",
-        "jsdoc",
-        "json",
-        "vim",
-        "vimdoc",
-        "markdown",
-        "markdown_inline",
-        "dockerfile",
-      },
-      highlight = {
-        enable = true,
-      },
-      indent = { enable = true },
-      autotag = {
-        enable = true,
-        enable_close_on_slash = false,
-      },
-    })
-  end,
+		treesitter.setup({
+			ensure_installed = {
+				"cpp",
+				"python",
+				"lua",
+				"java",
+				"javascript",
+				"typescript",
+				"html",
+				"css",
+				"php",
+				"tsx",
+				"jsdoc",
+				"json",
+				"vim",
+				"vimdoc",
+				"markdown",
+				"markdown_inline",
+				"dockerfile",
+			},
+			highlight = {
+				enable = true,
+			},
+			indent = { enable = true },
+			autotag = {
+				enable = true,
+				enable_close_on_slash = false,
+			},
+		})
+	end,
 }
