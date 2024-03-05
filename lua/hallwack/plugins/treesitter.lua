6efe90e200a804cf54868bbe0c1aceeaeb4edbb7
Raihan Adam <hallwack.id@gmail.com>
2024-03-05 13:57:49 +0700

    fix: fixing formatting

--- lua/hallwack/plugins/treesitter.lua
+++ lua/hallwack/plugins/treesitter.lua
@@ -1,23 +1,41 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-	},
-	config = function()
-		local treesitter = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
 
-		treesitter.setup({
-			ensure_installed = { "cpp", "python", "lua", "java", "javascript", "typescript", "html", "css" },
-			highlight = {
-				enable = true,
-			},
-			indent = { enable = true },
-			autotag = {
-				enable = true,
-				enable_close_on_slash = false
-			},
-		})
-	end,
+    treesitter.setup({
+      ensure_installed = {
+        "cpp",
+        "python",
+        "lua",
+        "java",
+        "javascript",
+        "typescript",
+        "html",
+        "css",
+        "php",
+        "tsx",
+        "jsdoc",
+        "json",
+        "vim",
+        "vimdoc",
+        "markdown",
+        "markdown_inline",
+        "dockerfile",
+      },
+      highlight = {
+        enable = true,
+      },
+      indent = { enable = true },
+      autotag = {
+        enable = true,
+        enable_close_on_slash = false,
+      },
+    })
+  end,
 }
