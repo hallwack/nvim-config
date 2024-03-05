cd753a97e6d720f1f958e2995ae0e7c8a4afe98d
Raihan Adam <hallwack.id@gmail.com>
2024-03-05 16:25:51 +0700

    fix: fixing mason lsp

--- lua/hallwack/plugins/lsp/mason.lua
+++ lua/hallwack/plugins/lsp/mason.lua
@@ -1,83 +1,97 @@
 return {
-  "williamboman/mason.nvim",
-  cmd = "Mason",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":MasonUpdate",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    {
-      "mrcjkb/rustaceanvim",
-      version = "3",
-      ft = { "rs" },
-      config = function()
-        vim.g.rustaceanvim = {
-          server = {
-            settings = {
-              ["rust-analyzer"] = {
-                diagnostics = {
-                  disabled = { "unresolved-proc-macro" },
-                },
-              },
-            },
-          },
-        }
-      end,
-    },
-  },
-  config = function()
-    local mason = require("mason")
-    local masonlsp = require("mason-lspconfig")
-    local lspconfig = require("lspconfig")
+	"williamboman/mason.nvim",
+	cmd = "Mason",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":MasonUpdate",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		{
+			"mrcjkb/rustaceanvim",
+			version = "3",
+			ft = { "rs" },
+			config = function()
+				vim.g.rustaceanvim = {
+					server = {
+						settings = {
+							["rust-analyzer"] = {
+								diagnostics = {
+									disabled = { "unresolved-proc-macro" },
+								},
+							},
+						},
+					},
+				}
+			end,
+		},
+	},
+	config = function()
+		local mason = require("mason")
+		local masonlsp = require("mason-lspconfig")
+		local lspconfig = require("lspconfig")
 
-    local default_setup = function(server)
-      lspconfig[server].setup({})
-    end
+		local default_setup = function(server)
+			lspconfig[server].setup({})
+		end
 
-    mason.setup({
-      ui = {
-        border = "rounded",
-      },
-    })
+		mason.setup({
+			ui = {
+				border = "rounded",
+			},
+		})
 
-    masonlsp.setup({
-      ensure_installed = { "lua_ls", "tsserver", "tailwindcss", "prismals", "html", "cssls" },
-      handlers = {
-        default_setup,
-        lua_ls = function()
-          lspconfig.lua_ls.setup({
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" },
-                },
-                workspace = {
-                  library = {
-                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-                    [vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy"] = true,
-                  },
-                  maxPreload = 100000,
-                  preloadFileSize = 10000,
-                },
-              },
-            },
-          })
-        end,
-        tailwindcss = function()
-          lspconfig.tailwindcss.setup({
-            settings = {
-              tailwindCSS = {
-                experimental = {
-                  classRegex = {
-                    { "clsx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
-                    { "cva\\(([^)]*)\\)",  "[\"'`]([^\"'`]*).*?[\"'`]" },
-                  },
-                },
-              },
-            },
-          })
-        end,
-      },
-    })
-  end,
+		masonlsp.setup({
+			ensure_installed = {
+				"lua_ls",
+				"tsserver",
+				"tailwindcss",
+				"prismals",
+				"html",
+				"cssls",
+				"svelte",
+				"astro",
+				"jsonls",
+        "quick_lint_js",
+        "intelephense",
+        "stylua",
+
+			},
+			handlers = {
+				default_setup,
+				lua_ls = function()
+					lspconfig.lua_ls.setup({
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = { "vim" },
+								},
+								workspace = {
+									library = {
+										[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+										[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+										[vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy"] = true,
+									},
+									maxPreload = 100000,
+									preloadFileSize = 10000,
+								},
+							},
+						},
+					})
+				end,
+				tailwindcss = function()
+					lspconfig.tailwindcss.setup({
+						settings = {
+							tailwindCSS = {
+								experimental = {
+									classRegex = {
+										{ "clsx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
+										{ "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+									},
+								},
+							},
+						},
+					})
+				end,
+			},
+		})
+	end,
 }
