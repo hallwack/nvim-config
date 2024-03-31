6f13730f24483dd48b76f08955326b6e018a86d8
Raihan Adam <hallwack.id@gmail.com>
2024-03-31 19:03:00 +0700

    fix: fixing init lua

--- init.lua
+++ init.lua
@@ -2,14 +2,19 @@ require("hallwack.init")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({ { import = "hallwack.plugins.lsp" }, { import = "hallwack.plugins" }, })
+require("lazy").setup({ { import = "hallwack.plugins.lsp" }, { import = "hallwack.plugins" }, {
+  import = "hallwack.vscode",
+  cond = (function()
+    return vim.g.vscode
+  end)
+} })
