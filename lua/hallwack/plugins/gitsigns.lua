d91bc001c75be5bcd9800c517e547bf06fc8d260
Raihan Adam <hallwack.id@gmail.com>
2024-04-01 07:27:35 +0700

    fix: fixing formatting and add keymaps for gitsigns

--- lua/hallwack/plugins/gitsigns.lua
+++ lua/hallwack/plugins/gitsigns.lua
@@ -1,29 +1,38 @@
 return {
-	{
-		"lewis6991/gitsigns.nvim",
-		ft = { "gitcommit", "diff" },
-		init = function()
-			vim.api.nvim_create_autocmd({ "BufRead" }, {
-				group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
-				callback = function()
-					vim.fn.system("git -C " .. '"' .. vim.fn.expand("%:p:h") .. '"' .. " rev-parse")
-					if vim.v.shell_error == 0 then
-						vim.api.nvim_del_augroup_by_name("GitSignsLazyLoad")
-						vim.schedule(function()
-							require("lazy").load({ plugins = { "gitsigns.nvim" } })
-						end)
-					end
-				end,
-			})
-		end,
-		config = function()
-			local gitsigns = require("gitsigns")
+  {
+    "lewis6991/gitsigns.nvim",
+    ft = { "gitcommit", "diff" },
+    init = function()
+      vim.api.nvim_create_autocmd({ "BufRead" }, {
+        group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
+        callback = function()
+          vim.fn.system("git -C " .. '"' .. vim.fn.expand("%:p:h") .. '"' .. " rev-parse")
+          if vim.v.shell_error == 0 then
+            vim.api.nvim_del_augroup_by_name("GitSignsLazyLoad")
+            vim.schedule(function()
+              require("lazy").load({ plugins = { "gitsigns.nvim" } })
+            end)
+          end
+        end,
+      })
+    end,
+    config = function()
+      local gitsigns = require("gitsigns")
 
-			gitsigns.setup({})
-		end,
-	},
-	{
-		"kdheepak/lazygit.nvim",
-		cmd = { "LazyGit" },
-	},
+      gitsigns.setup({
+        on_attach = function(bufnr)
+          local function map(mode, lhs, rhs, opts)
+            opts = vim.tbl_extend('force', { noremap = true, silent = true }, opts or {})
+            vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+          end
+
+          map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
+        end
+      })
+    end,
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = { "LazyGit" },
+  },
 }
