2d2ff033592e6b9161b9b54729351677e708c3d0
Raihan Adam <hallwack.id@gmail.com>
2024-05-29 11:57:30 +0700

    fix: add settings for nvim-ts-autotag

--- lua/hallwack/plugins/treesitter.lua
+++ lua/hallwack/plugins/treesitter.lua
@@ -1,45 +1,56 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-		{
-			"nvim-treesitter/nvim-treesitter-context",
-		},
-	},
-	config = function()
-		local treesitter = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  dependencies = {
+    {
+      "windwp/nvim-ts-autotag",
+      config = function()
+        require('nvim-ts-autotag').setup({
+          opts = {
+            enable_close = true,          -- Auto close tags
+            enable_rename = true,         -- Auto rename pairs of tags
+            enable_close_on_slash = false -- Auto close on trailing </
+          },
+        })
+      end
+    },
+    {
+      "nvim-treesitter/nvim-treesitter-context",
+    },
+  },
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
 
-		treesitter.setup({
-			ensure_installed = {
-				"cpp",
-				"python",
-				"lua",
-				"java",
-				"javascript",
-				"typescript",
-				"html",
-				"css",
-				"php",
-				"tsx",
-				"jsdoc",
-				"json",
-				"vim",
-				"vimdoc",
-				"markdown",
-				"markdown_inline",
-				"dockerfile",
+    treesitter.setup({
+      ensure_installed = {
+        "cpp",
+        "python",
+        "lua",
+        "java",
+        "javascript",
+        "typescript",
+        "html",
+        "css",
+        "php",
+        "tsx",
+        "jsdoc",
+        "json",
+        "vim",
+        "vimdoc",
+        "markdown",
+        "markdown_inline",
+        "dockerfile",
         "svelte"
-			},
-			highlight = {
-				enable = true,
-			},
-			indent = { enable = true },
-			autotag = {
-				enable = true,
-				enable_close_on_slash = false,
-			},
-		})
-	end,
+      },
+      highlight = {
+        enable = true,
+      },
+      indent = { enable = true },
+      autotag = {
+        enable = true,
+        enable_close_on_slash = false,
+      },
+    })
+  end,
 }
