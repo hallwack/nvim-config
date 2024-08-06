338a022a22a0b4715621c1f27221dcd1e7da329c
Raihan Adam <hallwack.id@gmail.com>
2024-08-06 17:13:41 +0700

    fix: add mdx.nvim for mdx syntax treesitter

--- lua/hallwack/plugins/treesitter.lua
+++ lua/hallwack/plugins/treesitter.lua
@@ -1,57 +1,65 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    {
-      "windwp/nvim-ts-autotag",
-      config = function()
-        require('nvim-ts-autotag').setup({
-          opts = {
-            enable_close = true,          -- Auto close tags
-            enable_rename = true,         -- Auto rename pairs of tags
-            enable_close_on_slash = false -- Auto close on trailing </
-          },
-        })
-      end
+return
+{
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      {
+        "windwp/nvim-ts-autotag",
+        config = function()
+          require('nvim-ts-autotag').setup({
+            opts = {
+              enable_close = true,          -- Auto close tags
+              enable_rename = true,         -- Auto rename pairs of tags
+              enable_close_on_slash = false -- Auto close on trailing </
+            },
+          })
+        end
+      },
+      {
+        "nvim-treesitter/nvim-treesitter-context",
+        config = function()
+          require('treesitter-context').setup({
+            mode = 'cursor'
+          })
+        end
+      },
     },
-    {
-      "nvim-treesitter/nvim-treesitter-context",
-      config = function()
-        require('treesitter-context').setup({
-          mode = 'cursor'
-        })
-      end
-    },
-  },
-  config = function()
-    local treesitter = require("nvim-treesitter.configs")
+    config = function()
+      local treesitter = require("nvim-treesitter.configs")
 
-    treesitter.setup({
-      ensure_installed = {
-        "cpp",
-        "python",
-        "lua",
-        "java",
-        "javascript",
-        "typescript",
-        "html",
-        "css",
-        "php",
-        "tsx",
-        "jsdoc",
-        "json",
-        "vim",
-        "vimdoc",
-        "markdown",
-        "markdown_inline",
-        "dockerfile",
-        "svelte"
-      },
-      highlight = {
-        enable = true,
-      },
-      indent = { enable = true },
-    })
-  end,
+      treesitter.setup({
+        ensure_installed = {
+          "cpp",
+          "python",
+          "lua",
+          "java",
+          "javascript",
+          "typescript",
+          "html",
+          "css",
+          "php",
+          "tsx",
+          "jsdoc",
+          "json",
+          "vim",
+          "vimdoc",
+          "markdown",
+          "markdown_inline",
+          "dockerfile",
+          "svelte"
+        },
+        highlight = {
+          enable = true,
+        },
+        indent = { enable = true },
+      })
+    end,
+  },
+  {
+    "davidmh/mdx.nvim",
+    config = true,
+    dependencies = { "nvim-treesitter/nvim-treesitter" }
+  }
 }
